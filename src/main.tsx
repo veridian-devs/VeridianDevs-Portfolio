0e49271e0dcf618047c81b3a934a2cb7ae6972f1
Shivam Saxena <shiv@Shivams-MacBook-Pro.local>
2025-10-30 21:12:08 +0530

    feat: add @vercel/speed-insights dependency and integrate SpeedInsights component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.scss'
 import { BrowserRouter } from 'react-router'
 import AppRouter from './config/Router.tsx'
-
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <SpeedInsights />
       <AppRouter/>
     </BrowserRouter>
   </StrictMode>
