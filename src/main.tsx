baf4a1f3d6ee10c5ce3bbfc5dfc19c2968ed6610
Shivam Saxena <shiv@Shivams-MacBook-Pro.local>
2025-10-31 21:11:57 +0530

    feat: add @vercel/analytics dependency and integrate Analytics component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.scss'
 import { BrowserRouter } from 'react-router'
 import AppRouter from './config/Router.tsx'
+import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <SpeedInsights />
+      <Analytics />
       <AppRouter/>
     </BrowserRouter>
   </StrictMode>
